a1db69d9d9b1a031a772b3a4549b80e0d3edc27f
Thiago Matulionis <thiago.mat.santos@gmail.com>
2022-06-24 17:18:31 -0300

    features calculator improvemments

--- src/main/java/br/com/erudio/MathController.java
+++ src/main/java/br/com/erudio/MathController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.erudio.exception.UnsupportedMathOperationException;
+import br.com.erudio.services.MathService;
 import br.com.erudio.utils.MathUtils;
 
 @RestController
@@ -14,6 +14,30 @@ public class MathController {
 	@RequestMapping(value = "/sum/{x}/{y}", method = RequestMethod.GET)
 	public Double sum(@PathVariable("x") String x, @PathVariable("y") String y) {
 		return MathUtils.convertToDouble(x) + MathUtils.convertToDouble(y);
+	}
+	
+	@RequestMapping(value = "/subtract/{x}/{y}", method = RequestMethod.GET)
+	public Double subtract(@PathVariable("x") String x, @PathVariable("y") String y) {
+		return MathUtils.convertToDouble(x) - MathUtils.convertToDouble(y);
+	}
+	
+	@RequestMapping(value = "/multiply/{x}/{y}", method = RequestMethod.GET)
+	public Double multiply(@PathVariable("x") String x, @PathVariable("y") String y) {
+		return MathUtils.convertToDouble(x) * MathUtils.convertToDouble(y);
+	}
+	
+	@RequestMapping(value = "/divide/{x}/{y}", method = RequestMethod.GET)
+	public Double divide(@PathVariable("x") String x, @PathVariable("y") String y) {
+		return MathUtils.convertToDouble(x) / MathUtils.convertToDouble(y);
+	}
+	
+	@RequestMapping(value = "/expression/{x}", method = RequestMethod.GET)
+	public Double expression(@PathVariable("x") String x) {
+		System.out.println("Received a request to calculate " + x);
+		
+		Double result = MathService.calculateExpression(x);
+		
+		return result;
 	}
 	
 }
--- src/main/java/br/com/erudio/utils/MathUtils.java
+++ src/main/java/br/com/erudio/utils/MathUtils.java
@@ -3,6 +3,9 @@ package br.com.erudio.utils;
 import br.com.erudio.exception.UnsupportedMathOperationException;
 
 public class MathUtils {
+	
+	private MathUtils() {}
+	
 	public static Double convertToDouble(String n) throws NumberFormatException {
 		if(n == null) return 0D;
 		
@@ -12,4 +15,31 @@ public class MathUtils {
 		throw new UnsupportedMathOperationException("Please, send 2 numeric values");
 	}
 	
+	public static Double calculate(char operation, Double[] numbers) {
+		if(numbers.length != 2) throw new UnsupportedMathOperationException("Expected exactly 2 numbers");
+		
+		switch (operation) {
+			case '+':
+				return numbers[0] + numbers[1];
+			case '-':
+				return numbers[0] - numbers[1];
+			case '*':
+				return numbers[0] * numbers[1];				
+			case '~':
+				return numbers[0] / numbers[1];		
+	
+			default:
+				throw new UnsupportedMathOperationException("Unsupported math operator");
+		}
+	}
+	
+	public static char findOperationInString(String expression) {
+		char[] listExpression = expression.toCharArray();
+		
+		for (char c : listExpression) {
+			if(c == '+' || c == '-' || c == '*' || c == '~') return c;
+		}
+		
+		throw new UnsupportedMathOperationException("Unable to detect one supported operator");
+	}
 }
